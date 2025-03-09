70465cfd8c10078a9b9185749c85ea90ed4a4bc3
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2025-03-09 15:54:25 -0500

    Added Project User relation in db

--- apps/primary-backend/index.ts
+++ apps/primary-backend/index.ts
@@ -3,5 +3,34 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
+
+// for creating a project
+app.post("/project", async (req, res) => {
+    const { prompt } = req.body;
+
+    //TODO: add logic to get a useful name for the project from the prompt
+
+    const description = prompt.split("\n")[0];
+
+    const project = await prismaClient.project.create({
+        data: { description }
+    });
+
+    res.json({ projectId: project.id });
+});
+
+app.get("/project/:projectId", async (req, res) => {
+    const { projectId } = req.params;
+    const project = await prismaClient.project.findUnique({
+        where: { id: projectId },
+    })
+    res.json(project);
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on Port 3000")
+})
